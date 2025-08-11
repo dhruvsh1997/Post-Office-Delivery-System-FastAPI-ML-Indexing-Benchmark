6151cdcdccbf8a4bf0f4e095eb8f881beb0b0bb0
dhruvsh1997 <dhruvsh1997@gmail.com>
2025-08-12 00:18:21 +0530

    RunSetup Updated

--- db.py
+++ db.py
@@ -1,10 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
 # PostgreSQL connection URL
-DATABASE_URL = "postgresql://postgres:9899@localhost/delivery_db"#db instead of localhost
+# DATABASE_URL = "postgresql://postgres:9899@localhost/delivery_db"#db instead of localhost
+
+# SQLite connection URL
+script_dir = os.path.dirname(__file__)
+DATABASE_URL = f"sqlite:///{os.path.join(script_dir, 'delivery_db.db')}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+
