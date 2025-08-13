1ee7922da230218ddb8c4933256b6c1cca1a2b33
dhruvsh1997 <dhruvsh1997@gmail.com>
2025-08-13 12:09:11 +0530

    train updated

--- ml/train_model.py
+++ ml/train_model.py
@@ -50,10 +50,10 @@ y = df["TARGET"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# --- Train model ---
+# ---- Train model ----
 model = RandomForestRegressor()
 model.fit(X_train, y_train)
 
-# --- Save model and encoders ---
+# ---- Save model and encoders ----
 joblib.dump(model, "ml/delivery_time_predictor.pkl")
 joblib.dump(encoders, "ml/encoders.pkl")
